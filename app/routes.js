c0cc1e5246725d69610c24984bf5210682d4ccb8
Hank92 <hongjik92@daum.net>
2016-04-24 00:48:38 -0400

    first commit

---
+++ app/routes.js
@@ -0,0 +1,204 @@
+// app/routes.js
+
+var request = require('request');
+var cheerio = require('cheerio');
+var mongoose = require('mongoose');
+var	methodOverride = require('method-override');
+
+
+var postModel = require('../app/models/post');
+
+module.exports = function (app, passport){
+
+app.param('id', function(req, res, next, id){
+	postModel.findById(id, function(err, docs){
+		if(err) res.json(err);
+		else
+			{
+				req.postId = docs;
+				next();
+			}
+			});	
+});
+
+
+app.get('/:id', function(req, res){
+	var postId = req.postId;
+	postId.userComments.push({ userPost: req.query.userPost});
+	postId.save();
+	res.render('individualHumor_Board.ejs', {postModel: postId});
+	console.log(postId)//finds the matching object
+});
+
+//post a comment on humor board
+app.post('/:id', function (req, res){
+	postModel.find({_id: req.params.id}, function(err, item){
+		if(err) return next("error finding blog post.");
+		item[0].userComments.push({userPost : req.body.userPost})
+		item[0].save(function(err, data){
+			if (err) res.send(err)
+			else res.redirect('/humor_board/' +req.params.id)
+		});
+	})
+
+}) //app.post  
+
+app.get('/', function (req, res){
+
+	var currentPage = 1;
+	if (typeof req.query.page !== 'undefined') {
+        currentPage = +req.query.page;
+    	}
+			postModel.paginate({}, {sort: {"_id":-1}, page: currentPage, limit: 9 }, function(err, results) {
+         if(err){
+         console.log("error");
+         console.log(err);
+     } else {
+    	    pageSize = results.limit;
+            pageCount = (results.total)/(results.limit);
+    		pageCount = Math.ceil(pageCount);
+    	    totalPosts = results.total;
+    	console.log(results.docs)
+
+    	res.render('humor_board.ejs', {
+    		postModels: results.docs,
+    		pageSize: pageSize,
+    		pageCount: pageCount,
+    		totalPosts: totalPosts,
+    		currentPage: currentPage
+    	})//res.render
+     }//else
+     });//paginate
+	
+});
+
+};
+
+request('http://bhu.co.kr/bbs/board.php?bo_table=best&page=1', function(err, res, body){
+	
+	if(!err && res.statusCode == 200) {
+		
+		var $ = cheerio.load(body);
+		$('td.subject').each(function(){
+		var bhuTitle = $(this).find('a font').text();
+		var newHref = $(this).find('a').attr('href');
+		newHref = newHref.replace("≀","&");
+		newHref = newHref.replace("id","wr_id");
+		newHref = newHref.replace("..",".");
+		var bhuUrl = "http://www.bhu.co.kr"+ newHref;
+	 	
+			request(bhuUrl, function(err, res, body){
+				if(!err && res.statusCode == 200) {
+				var $ = cheerio.load(body);
+				var comments = [];
+				var image_url = [];
+
+				$('span div img').each(function(){
+					var img_url = $(this).attr('src');
+					image_url.push(img_url);	
+				})
+				// scrape all the images for the post
+				
+					$("[style *= 'line-height: 180%']").each(function(){
+						var content =  $(this).text();
+							comments.push({content: content}); 	
+					})//scrape all the comments for the post
+
+					comments.splice(0,1)
+
+			postModel.find({title: bhuTitle}, function(err, newPosts){
+				
+				if (!newPosts.length){
+					//save data in Mongodb
+
+					var Post = new postModel({
+						title: bhuTitle,
+						url: bhuUrl,
+						image_url: image_url,
+						comments: comments
+					})
+			Post.save(function(error){
+					if(error){
+						console.log(error);
+					}
+					else 
+						console.log(Post);
+				})
+
+			//post.save
+				}//if bhuTitle안에 있는 {}
+
+			})//postModel.find
+			
+
+			}//if문
+
+			})//request
+
+			
+		});
+		
+	}//첫 if구문
+
+});
+
+request('http://issuein.com/', function(err, res, body){
+	
+	if(!err && res.statusCode == 200) {
+		
+		var $ = cheerio.load(body);
+		$('td.title').each(function(){
+		var issueTitle = $(this).find('a.hx').text();
+		var newHref = $(this).find('a').attr('href');
+		var issueUrl = "http://www.issuein.com"+ newHref;
+	 	
+			request(issueUrl, function(err, res, body){
+				if(!err && res.statusCode == 200) {
+				var $ = cheerio.load(body);
+				var image_url = [];
+
+				$('article div img').each(function(){
+					var img_url = $(this).attr('src');
+					image_url.push(img_url);	
+				})
+
+
+				// scrape all the images for the post
+				postModel.find({title: issueTitle}, function(err, newPosts){
+				
+				if (!newPosts.length){
+					//save data in Mongodb
+
+					var Post = new postModel({
+						title: issueTitle,
+						url: issueUrl,
+						image_url: image_url
+					
+					})
+			Post.save(function(error){
+					if(error){
+						console.log(error);
+					}
+					else 
+						console.log(Post);
+				})
+
+			//post.save
+				}//if bhuTitle안에 있는 {}
+
+			})//postModel.find
+			
+
+			}//if문
+
+			})//request
+
+			
+		});
+		
+	}//첫 if구문
+
+});
+
+
+
---
+++ server.js
@@ -0,0 +1,45 @@
+var request = require('request'),
+	cheerio = require('cheerio'),
+	express = require('express'),
+	app = express(),
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser'),
+	passport = require('passport'),
+	flash = require('connect-flash'),
+	methodOverride = require('method-override');
+
+	morgan = require('morgan'),
+	path = require('path'),
+	cookieParser = require('cookie-parser'),
+	session      = require('express-session');
+	mongoosePaginate = require('mongoose-paginate');
+
+//configuration//
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+
+	app.use(express.static(path.normalize(__dirname) + '/views'))
+	app.use(morgan('dev')); // log every request to the console
+	app.use(bodyParser());// pull information from html in POST
+	app.use(bodyParser.json());	 //setting app to use bodyParser
+	app.use(bodyParser.urlencoded());
+	app.use(cookieParser());
+	app.use(methodOverride());
+
+	app.use(session({ secret: 'iloveyousomuchtodeath' })); // session secret	app.use(passport.initialize());
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(flash());
+
+	app.set('views', path.normalize(__dirname) + '/views/html');
+	app.set('view engine', 'html')
+	app.set('view engine', 'ejs'); //set up ejs for templating
+
+// routes ======================================================================
+require('./app/routes.js')(app,passport); // load our routes and pass in our app
+
+//start the server
+app.listen(3000, function(){
+	console.log('Its running');
+})
